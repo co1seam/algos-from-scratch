c9c00856c1481718a9398359da4477fc22a5a313
co1seam <ginshy@yandex.ru>
2026-02-28 15:22:20 +0300

    feat(reversestring): Add solution for ReverseString(problem 2)
    - Implement two-pointer approach with O(n) time and O(n) space
    - Add table-driven tests

---
+++ problems/0002-reverse-string/solution.go
@@ -0,0 +1,18 @@
+// package reversestring provides a solution for the "Reverse String" problem.
+package reversestring
+
+// ReverseString reverses the order of elements in the given byte slice in place.
+// it does not allocate new slice, the function modifies the underlying array of the slice.
+// no changes are made, if the slice are empty or contains a single element.
+//
+// Time complexity: O(n)
+// Space complexity: O(n)
+func ReverseString(s []byte) {
+	left, right := 0, len(s)-1
+
+	for left <= right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
---
+++ problems/0002-reverse-string/solution_test.go
@@ -0,0 +1,25 @@
+package reversestring
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseString(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []byte
+		want  []byte
+	}{
+		{"basic case", []byte{'h', 'e', 'l', 'l', 'o'}, []byte{'o', 'l', 'l', 'e', 'h'}},
+		{"palindrome", []byte{'H', 'a', 'n', 'n', 'a', 'h'}, []byte{'h', 'a', 'n', 'n', 'a', 'H'}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReverseString(tt.input)
+			if !reflect.DeepEqual(tt.input, tt.want) {
+				t.Errorf("ReverseString() = %v, want %v", tt.input, tt.want)
+			}
+		})
+	}
+}
