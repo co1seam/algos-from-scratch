0d0194858abdfd5f224cc1ce52b72f20245bb5af
co1seam <ginshy@yandex.ru>
2026-02-28 13:32:37 +0300

    doc(twosum): Add go-doc comment

--- problems/00001-two-sum/solution.go
+++ problems/00001-two-sum/solution.go
@@ -1,15 +1,20 @@
 // package twosum provides a solution for the "Two Sum" problem.
 package twosum
 
+// TwoSum finds two numbers in nums array that add up to target and return their indicies.
+// it return nil if no such pair exists.
+//
+// Time complexity: O(n)
+// Space complexity: O(n)
 func TwoSum(nums []int, target int) []int {
-    seen := make(map[int]int)
+	seen := make(map[int]int)
 
-    for i, num := range nums {
-        if j, ok := seen[target-num]; ok {
-            return []int{j, i}
-        }
-        seen[num] = i
-    }
+	for i, num := range nums {
+		if j, ok := seen[target-num]; ok {
+			return []int{j, i}
+		}
+		seen[num] = i
+	}
 
-    return nil
+	return nil
 }
